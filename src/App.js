339a437a47a31bd6aaf85c90aa07dc280010fdf6
Mohammed Muti <mohammedmuti74@gamil.com>
2024-01-16 13:06:12 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Owner from "./Pages/Owner/Owner";
 import AddProperty from "./Pages/AddProperty/AddProperty";
 import ScrollToTop from "./Common/ScrollToTop/ScrollToTop";
